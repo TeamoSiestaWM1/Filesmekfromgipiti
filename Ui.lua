2b4d85ca80ed870934a71a2f98f6fb2d163e2e3f
TeamoSiesta <139937079+TeamoSiestaWM1@users.noreply.github.com>
2026-02-15 18:33:15 +0700

    Update Ui.lua

--- Ui.lua
+++ Ui.lua
@@ -321,6 +321,6 @@ for k, v in pairs(getgenv().Settings) do
 end
 
 task.spawn(function()
-    while wait(60) do if setfpscap then setfpscap(15) end end
+    while true do if setfpscap then setfpscap(15) end end
     pcall(function() loadstring(game:HttpGet("https://raw.githubusercontent.com/1toop/bss/refs/heads/main/pot.lua"))() end)
 end)
